5011484c706e2ea4ce2d09a5b190ae9d77b45c1d
andradebyte <joao.andrade@1wes.com>
2026-06-09 17:36:02 -0300

    .

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ const withPWA = require("@ducanh2912/next-pwa").default({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  output: "standalone",
 };
 
 module.exports = withPWA(nextConfig);
